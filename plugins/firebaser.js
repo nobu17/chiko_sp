2f67e5c344cf9b7c6e9f26dabf9dd708252e19db
nobuhiro <lunacy1717@outlook.jp>
2019-05-07 22:56:33 +0900

    add firebase

---
+++ plugins/firebaser.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import { AppEnv } from '../config/appenv'
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: AppEnv.FIREBASE_API_KEY,
+  authDomain: AppEnv.FIREBASE_AUTH_DOMAIN,
+  databaseURL: AppEnv.FIREBASE_DATABASE_URL,
+  projectId: AppEnv.FIREBASE_PROJECT_ID,
+  storageBucket: AppEnv.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: AppEnv.FIREBASE_MESSAGING_SENDER_ID,
+  appId: AppEnv.FIREBASE_APP_ID
+}
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig)
+
+export default firebase
