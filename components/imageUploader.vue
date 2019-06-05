a658ae434ccdade83ee734e837799b36f1465977
nobuhiro <lunacy1717@outlook.jp>
2019-06-05 20:08:00 +0900

    change image upload way

--- components/imageUploader.vue
+++ components/imageUploader.vue
@@ -31,19 +31,11 @@ export default {
       required: true
     }
   },
-  mounted() {
-    // console.log('iiii', this.value)
-    // this.base64 = this.value.fileUrl
-    // this.fileName = this.value.fileName
-  },
+  mounted() {},
   computed: {},
   data() {
-    return {
-      // base64: ''
-      // fileName: ''
-    }
+    return {}
   },
-
   methods: {
     // ボタンの有効状態の切り替わり
     disabledChanged(disabled) {
@@ -51,39 +43,7 @@ export default {
     },
     resizeFinish({ base64, imageName }) {
       // 親にイベント発行
-      // console.log(base64);
-      // this.fileUrl = base64
-      // this.fileName = imageName
-      // this.$emit('input', { fileUrl: this.base64, fileName: this.fileName })
       this.$emit('update', { fileUrl: base64, fileName: imageName })
-    },
-
-    drawImageArgs(image) {
-      const maxHeight = 900
-      const maxWidth = 900
-      let dstWidth = 0
-      let dstHeight = 0
-      const imageWidth = image.width
-      const imageHeight = image.height
-      let ratio = 0.0
-      // 最大幅を超える場合比率を計算
-      if (imageWidth > imageHeight) {
-        if (imageWidth > maxWidth) {
-          ratio = maxWidth / imageWidth
-        }
-      } else if (imageHeight > maxHeight) {
-        ratio = maxHeight / imageHeight
-      }
-
-      if (ratio > 0) {
-        dstWidth = imageWidth * ratio
-        dstHeight = imageHeight * ratio
-      } else {
-        dstWidth = imageWidth
-        dstHeight = imageHeight
-      }
-
-      return [image, 0, 0, imageWidth, imageHeight, 0, 0, dstWidth, dstHeight]
     }
   }
 }
--- components/resizableImage.vue
+++ components/resizableImage.vue
@@ -7,14 +7,14 @@
       color="info"
       accept="image/jpeg, image/jpg, image/png"
       :disabled="disabled"
-      @change="resize()"
+      @change="fileUpload()"
     >
     <v-container class="mt-0 mb-0">
       <v-layout wrap>
-        <v-flex xs12 md2>
+        <v-flex xs12 md3>
           <v-btn color="info" @click="pickFile" :disabled="disabled">アップロード</v-btn>
         </v-flex>
-        <v-flex xs9 md7>
+        <v-flex xs12 md9>
           <v-text-field label="アップロードボタンを押下してください。" readonly v-model="imageName"></v-text-field>
         </v-flex>
         <v-flex xs3 md3 v-if="canDelete">
@@ -28,6 +28,7 @@
 </template>
 
 <script>
+import ImageUtil from '../lib/imageUtil'
 export default {
   props: {
     drawImageArgs: {
@@ -58,48 +59,21 @@ export default {
     deleteClicked() {
       this.$emit('deleteClicked')
     },
-    resize() {
+    async fileUpload() {
       this.disabled = true
       this.$emit('disabledChanged', this.disabled)
       const file = this.$refs.input.files[0]
-
       if (!file) {
         return
       }
-
-      // this.imageName = file.name;
-      const reader = new FileReader()
-
-      reader.onload = event => {
-        const canvas = document.createElement('canvas')
-        const ctx = canvas.getContext('2d')
-        const image = new Image()
-        image.crossOrigin = 'Anonymous'
-
-        image.onload = () => {
-          const drawImageArgs = this.drawImageArgs(image)
-
-          if (drawImageArgs.length === 9) {
-            canvas.width = drawImageArgs[7]
-            canvas.height = drawImageArgs[8]
-          }
-
-          ctx.drawImage(...drawImageArgs)
-
-          const base64 = canvas.toDataURL()
-          const imageName = file.name
-          this.$emit('resized', {
-            base64,
-            imageName
-          })
-          this.disabled = false
-          this.$emit('disabledChanged', this.disabled)
-        }
-
-        image.src = event.target.result
-      }
-
-      reader.readAsDataURL(file)
+      const base64 = await ImageUtil.getCompressImageDataUrl(file)
+      const imageName = file.name
+      this.$emit('resized', {
+        base64,
+        imageName
+      })
+      this.disabled = false
+      this.$emit('disabledChanged', this.disabled)
     }
   }
 }
