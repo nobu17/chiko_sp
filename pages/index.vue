fc0458c5e5a83a76facb9ee7fe0fc09c1cbad4aa
nobuhiro_inagawa <lunacy171710@yahoo.co.jp>
2020-10-18 22:20:33 +0900

    add go to eat campaign

--- pages/index.vue
+++ pages/index.vue
@@ -49,6 +49,12 @@
           <h3 class="article_title stripe">Promotion</h3>
         </v-card-title>
         <v-card-text>
+          <h2>
+          今ならGoToEatを利用するとかなりお得です。よろしくお願いいたします。
+          </br>
+          <a href="https://amp.retty.me/r/100001547492/">詳細はこちら</a>
+          </h2>
+          </br>
           <iframe
             src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fchicospice.jp%2Fposts%2F112615360534335&show_text=true&width=552&height=231&appId"
             width="100%"
